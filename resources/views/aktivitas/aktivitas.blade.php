c2d9af53fc1d866cfc967da9196d6b650112a062
tegarftr <tegar6772@email.com>
2024-05-07 09:59:49 +0700

    d

--- resources/views/aktivitas/aktivitas.blade.php
+++ resources/views/aktivitas/aktivitas.blade.php
@@ -6,12 +6,12 @@
 <div class="container">
     <nav class="navbar navbar-expand-lg " style="margin-top: 50px">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="custom-tabs-four-tab" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link @yield('activeBerjalan')" href="{{ route('berjalan') }}">Sedang Berjalan</a>
+                    <a class="nav-link @yield('activeBerjalan')" id="custom-tabs-four-home-tab" data-toggle="pill" href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home" aria-selected="true"">Sedang Berjalan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link @yield('activeSelesai')" href="{{ url('aktivitas') }}">Selesai</a>
+                    <a class="nav-link @yield('activeSelesai')" id="custom-tabs-four-yuk-tab" data-toggle="pill" href="#custom-tabs-four-yuk" role="tab" aria-controls="custom-tabs-four-profile" aria-selected="false">Selesai</a>
                 </li>
             </ul>
         </div>
